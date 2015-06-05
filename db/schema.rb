e0dbaebd1288ca1d958680af67846fcb9c9797f1
nseyedas <nseyedas@gmail.com>
2015-06-04 22:02:56 -0700

    implemented design for receipt page

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150605042736) do
+
+  create_table "purchases", force: true do |t|
+    t.string   "email"
+    t.integer  "amount"
+    t.string   "description"
+    t.string   "currency"
+    t.string   "customer_id"
+    t.string   "card"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
